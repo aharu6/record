c881abf251742e99634747b782832e8b8839a375
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-05-13 22:35:43 +0900

    speech

--- page.js
+++ page.js
@@ -3,15 +3,17 @@ const windowheight = window.innerHeight;
 
 const speechrecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new speechrecognition();
-  recognition.continuous = true;
-  recognition.lang = "ja-JP";
-  recognition.interimResults = false;
+recognition.continuous = true;
+recognition.lang = "ja-JP";
+recognition.interimResults = false;
 
-
-function showbubble() {
+recognition.addEventListener("result", (event) => {
   const bubble = document.createElement("div");
   bubble.classList.add("speech-bubble");
-  bubble.classList.add("content");
+
+  const textbox = document.createElement("div");
+  textbox.classList.add("content");
+  bubble.appendChild(textbox);
   document.body.appendChild(bubble);
 
   const xline = Math.floor(Math.random() * (windowwidth - bubble.offsetWidth));
@@ -21,13 +23,11 @@ function showbubble() {
   bubble.style.left = `${xline}px`;
   bubble.style.top = `${yline}px`;
   bubble.style.display = "block";
-}
 
-recognition.addEventListener("result",(event)=>{
-  showbubble();
-  const transcript = event.results[event.results.length -1][0].transcript;
-  const output = document.querySelector("content");
-  content.textContent = transcript
-
+  textbox.style.left = `${xline}px`;
+  textbox.styletop = `${yline}px`;
+  textbox.style.display = "block";
+  const transcript = event.results[event.results.length - 1][0].transcript;
+  textbox.textContent = transcript;
 });
 recognition.start();
